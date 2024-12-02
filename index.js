58b024c9cf93b42e7686068ba36526860a7bd8d0
Marcos Wiliam Freitas <93717528+owiliamfreitas@users.noreply.github.com>
2024-12-02 20:11:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log (
-    document.querySelectorAll("#inicio")
-)
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+})
